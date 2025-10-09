1fe6a6e1101c191d637f81c4883a1e91532a284c
Anook <44230962+Kristofer-Aaron@users.noreply.github.com>
2025-10-09 13:46:35 +0200

    structure rework, prototype UI commit
    Minor project structure rework, prototype UI progress upload (incomplete)

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Load head content from separate file !-->
+    <?php require 'partials/head.html' ?>
+</head>
+
+<body>
+    <!-- Load navbar content from separate file !-->
+    <nav>
+        <?php require 'partials/navbar.html' ?> 
+    </nav>
+
+    <div id="guitarNeck">
+
+    </div>
+</body>
+
+<!-- <script src="assets/js/script.js"></script> -->
+<script src="assets/js/controls.js"></script>
+</html>
