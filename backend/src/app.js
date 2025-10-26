8377f817be95e1886a86eb47491fe5b94514450e
akosgal <gal.akos.2005@gmail.com>
2025-10-27 00:19:41 +0100

    db.js, app.js, server.js

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import chordRoutes from './routes/chordRoutes.js';
+import tuningRoutes from './routes/tuningRoutes.js';
+import gripRoutes from './routes/gripRoutes.js';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/chords', chordRoutes);
+app.use('/api/tunings', tuningRoutes);
+app.use('/api/grips', gripRoutes);
+
+export default app;
