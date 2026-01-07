856b197b0533f11112ad4a185b20429cd759265f
Secret <96205404+Secret198@users.noreply.github.com>
2026-01-07 13:59:41 +0100

    Bootstrap initial commit

--- index.php
+++ index.php
@@ -7,21 +7,20 @@
 </head>
 
 <body>
-    <!-- Load navbar content from separate file !-->
-    <nav>
-        <?php require 'partials/navbar.html' ?> 
-    </nav>
+    <!-- Load navbar from separate file !-->
+    <?php require 'navbar-bootstrap.html' ?> 
+    
 
     <div id="fretboardWrapper">
         <div id="fretboardContainer"></div>
     </div>
     
-    <footer>
-        <?php require 'partials/footer.html' ?>
-    </footer>
+    <?php require 'footer-bootstrap.html' ?>
 </body>
 
 
+<!-- Bootstrap 5.3.8 script -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 <!-- <script src="assets/js/script.js"></script> -->
 <script src="assets/js/controls.js"></script>
